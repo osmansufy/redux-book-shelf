0c6a1b8e874af54bb874ddb60b4172d79f03120d
osmansufy <osmansufy20@gmail.com>
2021-05-26 20:38:54 +0600

    first-meat

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import Book from '../components/Book/Book';
-import books from '../fakeData/books.json'
-import PageLayout from '../components/PageLayout/PageLayout';
+import React, { useEffect, useState } from "react";
+import Book from "../components/Book/Book";
+import books from "../fakeData/books.json";
+import PageLayout from "../components/PageLayout/PageLayout";
+import axios from "axios";
 const Discover = () => {
-    return (
-        <PageLayout>
-            {
-                books.map((book) => (<Book key={book.id} book={book} />))
-            }
-        </PageLayout>
-    );
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/getAllBook")
+      .then((res) => {
+        setAllBooks(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <PageLayout>
+      {allBooks.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </PageLayout>
+  );
 };
 
 export default Discover;
