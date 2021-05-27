c906452c0e9a6711de2d50e5ba2b8272ee8a04d3
osmansufy <osmansufy20@gmail.com>
2021-05-27 20:12:53 +0600

    day2update

--- src/pages/ReadingList.js
+++ src/pages/ReadingList.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageLayout from '../components/PageLayout/PageLayout';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import SingleBook from "../components/Book/Book";
+import PageLayout from "../components/PageLayout/PageLayout";
 
-const ReadingList = () => {
-    return (
-        <PageLayout>
-             <p>Looks like you've finished all your books! Check them out in your <Link to="finish">finished books</Link> or <Link to="/">discover more</Link>.</p>
-        </PageLayout>
-    );
+const ReadingList = (props) => {
+  const readingBooks = useSelector((state) => state.books.readingList);
+
+  useEffect(() => {
+    console.log(readingBooks);
+  }, []);
+  return (
+    <PageLayout>
+      {readingBooks?.length ? (
+        readingBooks?.map((book) => <SingleBook key={book.id} book={book} />)
+      ) : (
+        <p>
+          Looks like you've finished all your books! Check them out in your{" "}
+          <Link to="finish">finished books</Link> or{" "}
+          <Link to="/">discover more</Link>.
+        </p>
+      )}
+    </PageLayout>
+  );
 };
 
 export default ReadingList;
